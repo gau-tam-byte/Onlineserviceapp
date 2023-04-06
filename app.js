9ba79dc9b0a4b1d5c11d3fa943bcf14708345053
gau-tam-byte <gautamkumark1214@gmail.com>
2023-04-07 02:36:42 +0530

    finalcode

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/',require('./routes/users'))
 app.use(express.static(path.join(__dirname, "./client/build")))
 
 app.get('*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname,'./client/build/index.html'))
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
 // if (process.env.NODE_ENV === 'production') {
