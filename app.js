1df8c187f1dddf6293ae931dd06ba53491636be4
gau-tam-byte <gautamkumark1214@gmail.com>
2023-04-07 00:55:25 +0530

    finalcode

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const passport = require('passport');
 require('../Backend/db/dbconnection');
 const dotenv = require('dotenv')
 dotenv.config({path:'./config.env'})
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({path:'./config.env'})
+// const mongoose = require('mongoose')
+
+const db = process.env.DATABASE
+// const db = 'mongodb://127.0.0.1:27017/?directConnection=true';
+mongoose.connect(db).then(()=>{
+  console.log("connection success")
+}).catch((err) =>{
+  console.log(console.error(err),"connection not sucess")
+})
+
 // const path = require('path')
 // const mongoose = require('mongoose');
 // const morgan = require('morgan')
