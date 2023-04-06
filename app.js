14727f2b6d67634351b199a71d4f4e8142bd0c75
gau-tam-byte <gautamkumark1214@gmail.com>
2023-04-07 00:05:12 +0530

    finalcode

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const cookieparser = require('cookie-parser');
+const session = require('express-session')
+const passport = require('passport');
+require('../Backend/db/dbconnection');
+const dotenv = require('dotenv')
+dotenv.config({path:'./config.env'})
+const path = require('path')
+// const mongoose = require('mongoose');
+// const morgan = require('morgan')
+// const expressHandlerbars = require('express-handlebars')
+// const flash = require('connect-flash');
+// mongoose.Promise= global.Promise
+// mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true')
+
+// 1 
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieparser())
+app.use(session({
+  cookie: { maxAge: 1000000 },
+  secret: 'connect.sid',
+  saveUninitialized: true,
+  resave: false,
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
+
+app.use('/',require('./routes/index'))
+app.use('/',require('./routes/users'))
+
+// app.use(express.static(path.join(__dirname, './client')))
+
+// app.get('*', (req,res)=>{
+//   res.sendFile(path.join(__dirname,'./client/build/index.html'))
+// })
+
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
+
+const port = process.env.PORT
+app.listen(port, () => console.log('Server started listening on port 5000!' ,{port}))
